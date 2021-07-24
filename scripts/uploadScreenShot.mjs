e4044f76f6f1086478a848b2a88d0d7d8e866ed0
tubone24 <tubo.yyyuuu@gmail.com>
2021-07-24 12:40:29 +0900

    fix mjs

--- scripts/uploadScreenShot.mjs
+++ scripts/uploadScreenShot.mjs
@@ -1,4 +1,4 @@
-import { ApolloClient, gql, InMemoryCache } from '@apollo/client'
+import { ApolloClient, gql } from '@apollo/client'
 // "fetch" has not been found globally and no fetcher has been configured. To fix this, install a fetch package (like https://www.npmjs.com/package/cross-fetch), instantiate the fetcher, and pass it into your HttpLink constructor.
 import fetch from 'cross-fetch';
 import fs from 'fs'
@@ -12,7 +12,6 @@ const URI_ENDPOINT = "https://crisp-muskox-39.hasura.app/v1/graphql";
 
 const client = new ApolloClient({
   link: new HttpLink({ uri: URI_ENDPOINT, fetch })
-  cache: new InMemoryCache()
 });
 
 const base64Data = fs.readFileSync(filePath, { encoding: 'base64' });
